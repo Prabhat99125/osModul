38950a178e5c31f73df0fd37c0c6fef072102ab0
Prabhat Rathva <144404271+Prabhat99125@users.noreply.github.com>
2024-07-24 05:59:13 +0530

    Add files via upload

---
+++ os.js
@@ -0,0 +1,7 @@
+const os = require("os");
+// console.log(os.arch()); // show yor computer bits
+// console.log(os.freemem() / (1024 * 1024 * 1024)) // show unused ram
+// console.log(os.totalmem() / (1024 * 1024 * 1024)); show total ram
+// console.log(os.hostname());
+// console.log(os.platform());
+console.log(os.userInfo());
